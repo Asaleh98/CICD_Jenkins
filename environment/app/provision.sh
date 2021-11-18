25f73122e59447ab503caca3da996c89d9553759
Asaleh98 <89383713+Asaleh98@users.noreply.github.com>
2021-11-18 11:05:46 +0000

    Update provision.sh

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -11,7 +11,6 @@ sudo apt-get install python-software-properties -y
 #Finds a specific verison of Nodejs and installs it again and with pm2
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-sudo npm install pm2 -g
 
 #Creates a environment variable and stores in .bashrc file
 sudo echo 'export DB_HOST="mongodb://192.168.10.150:27017/posts"' >> .bashrc
